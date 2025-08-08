d424b5f9d7a3442140d580bbfec138d3426dd8e7
Kris <kristinaovchinnikova22@gmail.com>
2025-08-08 13:56:03 +0600

    delated command frontend dev in docker-compouse

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
+        "./src/**/*.{js,jsx,ts,tsx,vue,html}",
+        "./public/index.html",
         "./index.html",
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
